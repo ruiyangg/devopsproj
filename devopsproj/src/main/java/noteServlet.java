f1894870590e4cf869cbfff843fedd5cb23df7c8
LeongRuiYang <2001782h@student.tp.edu.sg>
2022-02-06 17:45:03 +0800

    amended note servlet.java

--- devopsproj/src/main/java/noteServlet.java
+++ devopsproj/src/main/java/noteServlet.java
@@ -1,15 +1,25 @@
 
 
+
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
+
+import devopsproj.note;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 
 
 /**
@@ -19,6 +29,30 @@ import java.sql.PreparedStatement;
 public class noteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	 //Step 1: Prepare list of variables used for database connections
+	 private String jdbcURL = "jdbc:mysql://localhost:3306/notedetails";
+	 private String jdbcUsername = "root";
+	 private String jdbcPassword = "password";
+	 //Step 2: Prepare list of SQL prepared statements to perform CRUD to our database
+	 private static final String INSERT_NOTE_SQL = "INSERT INTO notedetails" + " (name, description, target_date, accomplish) VALUES " + " (?, ?, ?);";
+	 private static final String SELECT_NOTE_BY_ID = "select name,description,target_date,target_date from notedetails where name =?";
+	 private static final String SELECT_ALL_NOTE = "select * from notedetails ";
+	 private static final String DELETE_NOTE_SQL = "delete from notedetails where name = ?;";
+	 private static final String UPDATE_NOTE_SQL = "update notedetails set name = ?,notedetails= ?, target_date =?,accomplish =? where name = ?;";
+	 //Step 3: Implement the getConnection method which facilitates connection to the database via JDBC
+	 protected Connection getConnection() {
+	 Connection connection = null;
+	 try {
+	 Class.forName("com.mysql.jdbc.Driver");
+	 connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+	 } catch (SQLException e) {
+	 e.printStackTrace();
+	 } catch (ClassNotFoundException e) {
+	 e.printStackTrace();
+	 }
+	 return connection;
+	 }
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -33,6 +67,25 @@ public class noteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		String action = request.getServletPath();
+		 try {
+		 switch (action) {
+		 case "/noteServlet/delete":
+		 deletenote(request, response);
+		 break;
+		 case "/noteServlet/edit":
+		 showEditForm(request, response);
+		 break;
+		 case "/noteServlet/update":
+		 updatenote(request, response);
+		 break;
+		 case "/noteServlet/dashboard":
+		 listnote(request, response);
+		 break;
+		 }
+		 } catch (SQLException ex) {
+		 throw new ServletException(ex);
+		 } 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -44,8 +97,7 @@ public class noteServlet extends HttpServlet {
 		
 		response.setContentType("text/html");
 		
-		//Step 1: Initialize a PrintWriter object to return the html values via the response
-		PrintWriter out = response.getWriter();
+		//Step 1: Initialize a PrintWriter object to return the html values via the response PrintWriter out = response.getWriter();
 		//Step 2: retrieve the four parameters from the request from the web form
 		String n = request.getParameter("userName");
 		String p = request.getParameter("description");
@@ -67,20 +119,111 @@ public class noteServlet extends HttpServlet {
 		 int i = ps.executeUpdate();
 		//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response,
 		 if (i > 0){
-			 PrintWriter writer = response.getWriter();
-			 writer.println("<h1>" + "You have successfully added note!" +
-			 "</h1>");
-			 writer.close();
+			 response.sendRedirect("/devopsproj/notesServlet/dashboard");
 			 }
 			 }
 			 //Step 8: catch and print out any exception
 			 catch (Exception exception) {
 			  System.out.println(exception);
-			  out.close();
+			  System.out.close();
 			 }
 		 
 		
 		doGet(request, response);
 	}
+	//method to get parameter, query database for existing user data and redirect to user edit page
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, ServletException, IOException {
+	//get parameter passed in the URL
+	String name = request.getParameter("name");
+	note existingnote = new note("", "", "", "");
+	// Step 1: Establishing a Connection
+	try (Connection connection = getConnection();
+	// Step 2:Create a statement using connection object
+	PreparedStatement preparedStatement =
+	connection.prepareStatement(SELECT_NOTE_BY_ID);) {
+	preparedStatement.setString(1, name);
+	// Step 3: Execute the query or update query
+	ResultSet rs = preparedStatement.executeQuery();
+	// Step 4: Process the ResultSet object
+	while (rs.next()) {
+	name = rs.getString("name");
+	String description = rs.getString("description");
+	String target_date = rs.getString("target_date");
+	String accomplish = rs.getString("accomplish");
+	existingnote = new note(name, description, target_date, accomplish);
+	}
+	} catch (SQLException e) {
+	System.out.println(e.getMessage());
+	}
+	//Step 5: Set existingUser to request and serve up the userEdit form
+	request.setAttribute("note", existingnote);
+	request.getRequestDispatcher("/noteEdit.jsp").forward(request, response);
+	}
 
+	
+	//method to update the user table base on the form data
+	private void updatenote(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, IOException {
+	//Step 1: Retrieve value from the request
+	String oriName = request.getParameter("oriName");
+	 String name = request.getParameter("name");
+	 String description = request.getParameter("description");
+	 String target_date = request.getParameter("target_date");
+	 String accomplish = request.getParameter("accomplish");
+
+	 //Step 2: Attempt connection with database and execute update user SQL query
+	 try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(UPDATE_NOTE_SQL);) {
+	 statement.setString(1, name);
+	 statement.setString(2, description);
+	 statement.setString(3, target_date);
+	 statement.setString(4, accomplish);
+	 statement.setString(5, oriName);
+	 int i = statement.executeUpdate();
+	 }
+	 //Step 3: redirect back to UserServlet (note: remember to change the url to your project name)
+	 response.sendRedirect("http://localhost:8090/devopsproj/notesServlet/dashboard");
+	}
+	
+	//method to delete user
+	private void deletenote(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, IOException {
+	//Step 1: Retrieve value from the request
+	 String name = request.getParameter("note");
+	 //Step 2: Attempt connection with database and execute delete user SQL query
+	 try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_NOTE_SQL);) {
+	 statement.setString(1, name);
+	 int i = statement.executeUpdate();
+	 }
+	 //Step 3: redirect back to UserServlet dashboard (note: remember to change the url to your project name)
+	 response.sendRedirect("http://localhost:8090/devopsproj/notesServlet/dashboard");
+	}
+	
+	//Step 5: listUsers function to connect to the database and retrieve all users records
+	private void listnote(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, IOException, ServletException
+	{
+	List <note> note = new ArrayList <>();
+	 try (Connection connection = getConnection();
+	 // Step 5.1: Create a statement using connection object
+	 PreparedStatement preparedStatement =
+	connection.prepareStatement(SELECT_ALL_NOTE);) {
+	 // Step 5.2: Execute the query or update query
+	 ResultSet rs = preparedStatement.executeQuery();
+	 // Step 5.3: Process the ResultSet object.
+	 while (rs.next()) {
+	 String name = rs.getString("name");
+	 String description = rs.getString("description");
+	 String target_date = rs.getString("target_date");
+	 String accomplish = rs.getString("accomplish");
+	 note.add(new note(name, description, target_date, accomplish));
+	 }
+	 } catch (SQLException e) {
+	 System.out.println(e.getMessage());
+	 }
+	// Step 5.4: Set the users list into the listUsers attribute to be pass to th noteManagement.jsp
+	request.setAttribute("listnote", note);
+	request.getRequestDispatcher("/noteManagement.jsp").forward(request, response);
+	}
+	
 }
